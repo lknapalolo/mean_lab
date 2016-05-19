c3a68632d256762aee928db1799a7890ed0e9171
Spicy Legato <jpqreichard@gmail.com>
2016-05-19 12:21:45 -0400

    corrects reference and spacing

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,12 @@
-var mongoose = require("./connection")
+var mongoose    = require("./connection");
+var dataCorn    = require("./seeds.json");
 
-var Unicorn = mongoose.model("Unicorn");
-var Vegetable = mongoose.model("Vegetable");
-var Colors = mongoose.model("Colors");
+var Unicorn     = mongoose.model("Unicorn");
+var Vegetable   = mongoose.model("Vegetable");
+var Colors      = mongoose.model("Colors");
 
 Unicorn.remove({}).then(function(){
-  Unicorn.collection.insert(seed_data).then(function(){
-    process.exit()
-  })
-})
+  Unicorn.collection.insert(dataCorn).then(function(){
+    process.exit();
+  });
+});
