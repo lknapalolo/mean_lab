99a1956e1c1a636f9590b1abd9c56bdeb038393c
Spicy Legato <jpqreichard@gmail.com>
2016-05-19 17:23:02 -0400

    remvpes color schema ref

--- db/seed.js
+++ db/seed.js
@@ -3,7 +3,6 @@ var dataCorn    = require("./seeds.json");
 
 var Unicorn     = mongoose.model("Unicorn");
 var Vegetable   = mongoose.model("Vegetable");
-var Colors      = mongoose.model("Colors");
 
 Unicorn.remove({}).then(function(){
   Unicorn.collection.insert(dataCorn).then(function(){
