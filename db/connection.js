9d779cf451c31b99fc2331c94ac8051d2bdea93b
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-05-19 13:11:39 -0400

    fixes connection.js to seed shh_magical_secret_db

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,6 @@ var mongooseConnection = require("mongoose");
 
 mongooseConnection.connect("127.0.0.1:27017/shh_magical_secret_db");
 
-var Unicorn = new mongooseConnection.Schema({
-  name: String, 
-  magical_power: String,
-  colors: [Colors],
-  wings_type: String,
-  alicorn: [Vegetable],
-  nemesis: String
-});
 
 var Vegetable = new mongooseConnection.Schema({
   name: String,
@@ -21,8 +13,17 @@ var Colors = new mongooseConnection.Schema({
   percentage: String
 });
 
-mongooseConnection.model("Unicorn", Unicorn);
+var Unicorn = new mongooseConnection.Schema({
+  name: String,
+  magical_power: String,
+  colors: [Colors],
+  wings_type: String,
+  alicorn: [Vegetable],
+  nemesis: String
+});
+
 mongooseConnection.model("Vegetable", Vegetable);
 mongooseConnection.model("Colors", Colors);
+mongooseConnection.model("Unicorn", Unicorn);
 
 module.exports = mongooseConnection;
