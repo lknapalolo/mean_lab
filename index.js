c464e5b88777ceb477ab67bc77cca89d1ed75ac7
Michael Nahum <USGrant63@gmail.com>
2016-05-20 09:57:23 -0400

    added create, started update functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var mongoose        = require("./db/connection");
 var express         = require("express");
 var hbs             = require("express-handlebars");
+var parser          = require("body-parser");
 
 
 var app = express();
@@ -8,7 +9,8 @@ var app = express();
 var Unicorn = mongoose.model("Unicorn");
 var Vegetable = mongoose.model("Vegetable");
 
-app.use("/assets", express.static("public"))
+app.use(parser.json({extended: true}));
+app.use("/assets", express.static("public"));
 app.set("port", process.env.PORT || 7777);
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
@@ -21,19 +23,31 @@ app.engine(".hbs", hbs({
 
 app.get("/api/unicorns/:name", function(req, res){
   Unicorn.findOneAndUpdate(req.params, req.body, {new:true}).then(function(unicorn){
-    res.json(unicorn)
-  })
-})
+    res.json(unicorn);
+  });
+});
 
 app.get("/api/unicorns", function(req, res){
   Unicorn.find().then(function(unicorns_db){
-    res.json(unicorns_db)
-  })
-})
+    res.json(unicorns_db);
+  });
+});
+
+app.put("/api/unicorns/:_id", function(req, res){
+  Unicorn.findOneAndUpdate(req.params, req.body, {new: true}).then(function(unicorns_db){
+    res.json(unicorn);
+  });
+});
+
+app.post("/api/unicorns", function(req, res){
+  Unicorn.create(req.body).then(function(unicorn){
+    res.json(unicorn);
+  });
+});
 
 app.get("/*", function(req, res){
   res.render("layout-main", {layout:false})
-})
+});
 
 app.listen(7777, function(){
   console.log("UNICORNZ 🦄");
--- public/app.js
+++ public/app.js
@@ -41,6 +41,12 @@ IndexCtrl.$inject = ["Unicorn", "$stateParams", "$state"]
   function IndexCtrl(Unicorn, $stateParams, state){
     var vm = this;
     vm.unicorns = Unicorn.query();
+    vm.create = function(){
+      Unicorn.save(vm.newUnicorn, function(response){
+        console.log(response)
+        vm.unicorns.push(response);
+      });
+    }
   }
 
 ShowCtrl.$inject = ["Unicorn", "$stateParams", "$state"]
